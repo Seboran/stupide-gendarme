630c600a26aa402d9a33bfcdda84053364bdc7d7
Seboran <nirinarabeson@outlook.fr>
2017-01-07 00:26:39 +0100

    fonctionne sur messenger.com
    fallait rajouter quelque chsoe dans l'entête

--- Stupide Gendarme.user.js
+++ Stupide Gendarme.user.js
@@ -4,12 +4,14 @@
 // @version      0.1
 // @description  Parler sur messenger de façon automatisée ET intelligente
 // @author       Nirina Guillaume Stéphane
-// @match        http://*/*
+// @match        https://www.messenger.com
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 	alert("Hello world blbl");
+	var limite = 3;
+	
     // Your code here...
 })();
