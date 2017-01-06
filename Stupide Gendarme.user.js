4a6034ac87ed2ea31f207a73a8fcaece8d5b9ef9
Stéphane Maillot <s.maillot2@gmail.com>
2017-01-07 00:27:50 +0100

    Update Stupide Gendarme.user.js

--- Stupide Gendarme.user.js
+++ Stupide Gendarme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        BlblBot
 // @description Don't spend your day bullshitting on facebook, this bot do it for you
-// @include     https://www.messenger.com/*
+// @include     https://www.messenger.com/t/1239124672796689
 // @version     0.1
 // @grant       none
 // ==/UserScript==
