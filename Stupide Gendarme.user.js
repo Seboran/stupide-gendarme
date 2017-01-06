d7175830bad9cf5013c24a65ba6b1be871d89943
Seboran <nirinarabeson@outlook.fr>
2017-01-06 23:30:38 +0100

    Add files via upload

---
+++ Stupide Gendarme.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Stupide Gendarme
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Parler sur messenger de façon automatisée ET intelligente
+// @author       Nirina Guillaume Stéphane
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
