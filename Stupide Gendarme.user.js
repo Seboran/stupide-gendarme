76f6cd3a312be7bb3a81fdb5df6595359bbe56b8
Guillaume Perouffe <guillaume.perouffe@centrale-marseille.fr>
2017-01-08 18:22:07 +0100

    Ortographe

--- Stupide Gendarme.user.js
+++ Stupide Gendarme.user.js
@@ -1,5 +1,5 @@
 // @name        BlblBot
-// @description Don't spend your day bullshitting on facebook, this bot do it for you
+// @description Don't spend your day bullshitting on facebook, this bot does it for you
 // @include     https://www.messenger.com/t/1239124672796689
 // @version     0.1
 // @grant       none
